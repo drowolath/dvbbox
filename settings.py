8adf45c25a31ca1e087dd7ad9e94e2c5922844df
Thomas Ayih-Akakpo <thomas.ayih-akakpo@gulfsat.mg>
2016-06-29 14:32:08 +0300

    add CHANNELS as mandatory setting

--- settings.py
+++ settings.py
@@ -12,3 +12,20 @@ DATABASE = {
 MEDIA_FOLDERS = [
     '/somewhere/over/the/rainbow',
     ]
+
+CHANNELS = {
+    85: {
+        'name': 'My Channel',
+        'audio_pid': 30,
+        'video_pid': 31,
+        'vlc_telnet_port': 4000,
+        'udp_multicast': '239.54.100.1:1234'
+        },
+    86: {
+        'name': 'My Channel',
+        'audio_pid': 32,
+        'video_pid': 33,
+        'vlc_telnet_port': 4001,
+        'udp_multicast': '239.54.100.2:1234'
+        },
+    }
